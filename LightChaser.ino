ed0b4300e23ed740f5d1e38468e1441f429d6bb9
Alexander Leong <106341343+DryWater02@users.noreply.github.com>
2022-12-12 10:45:02 -0800

    Add files via upload

---
+++ LightChaser.ino
@@ -0,0 +1,44 @@
+/*
+* LightChaser
+*
+* This little vehicle is on an impossible, never ending 
+* mission to catch the light. You can make it follw you 
+* by pointing a flashligt at it. (The one on your mobile 
+* phone eg.) It will always turn towards the light. 
+* 
+* (c) 2013-2016 Arduino LLC.
+*/
+
+#include <EducationShield.h>
+
+//Declare the two wheels of robot, left wheel to D6 and
+// right wheel to D9
+Wheels wheels=Wheels(6, 9);
+
+//Declare the two LightSensor
+LightSensor sensorLeft= LightSensor(A0);
+LightSensor sensorRight= LightSensor(A2);
+
+void setup(){
+  //initialize the LightSensor
+  sensorLeft.config(600,800);
+  sensorRight.config(600,800);
+  Serial.begin(9600);
+
+  //initialize the servo motors
+  wheels.begin();
+}
+void loop(){
+  Serial.println(sensorLeft.getState());
+  Serial.println(sensorRight.getState());
+  if(sensorLeft.getState()){
+	//Left LightSensor detects strong light, the vechile turns left
+    wheels.turnLeft();
+  }else if(sensorRight.getState()){
+	//Right LightSensor detects strong light, the vechile turns right
+    wheels.turnRight();
+  }else{
+	//No strong light detected, the vechile goes straight
+    wheels.goForward();
+  }
+}
